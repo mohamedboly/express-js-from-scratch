45a7ca0e908295f10bbd34524cc22b6c76d8c5dc
mohamedboly <mohamedbolydev@gmail.com>
2024-12-20 16:27:00 +0000

    middleware update

--- 1 Intro/index.js
+++ 1 Intro/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 const app = express();
 
-app.get("youtube",  function (req, res) {
+app.get("/youtube",  function (req, res) {
   res.send(`Bienvenue G5`);
 });
 app.get("/about", function (req, res){
   res.send(`Cette page est consacree a mon parcours`);
 });
 
-app.get("/projets", (req, res) => {
+app.get("/projets", function (req, res)  {
   res.send(`la page de mes projets`);
+});
+
+app.get("/etudiants/:id/:niveau", (req, res) => {
+  res.send(`etudiant avec l'id : `+req.params.id + " et le niveau "+ req.params.niveau);
 });
 
 
